216bf5f51d898940fdd9b406d2b8682a6909bae1
markgolubev <markflyingbear@gmail.com>
2023-02-09 13:46:18 +0000

    Initial Commit

---
+++ coreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataTest
+//
+//  Created by Марк Голубев on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
